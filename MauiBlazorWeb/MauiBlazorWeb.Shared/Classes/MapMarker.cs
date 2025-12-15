f4e74e999485363c87143be98289f9718ff518bf
Sandro Räss <s.raess@me.com>
2025-12-15 20:36:24 +0100

    Add Map for SaveChickenRequests.

---
+++ MauiBlazorWeb/MauiBlazorWeb.Shared/Classes/MapMarker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBlazorWeb.Shared.Classes
+{
+    public class MapMarker
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Info { get; set; } = string.Empty;
+    }
+}
--- MauiBlazorWeb/MauiBlazorWeb.Shared/wwwroot/js/map.js
+++ MauiBlazorWeb/MauiBlazorWeb.Shared/wwwroot/js/map.js
@@ -1,9 +1,34 @@
-﻿window.showMap = (lat, lon) => {
-    var map = L.map('map').setView([lat, lon], 15);
+﻿let map;
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap'
-    }).addTo(map);
+function initMap(initialMarkers) {
+    if (!map) {
+        map = L.map('map').setView([0, 0], 2);
 
-    L.marker([lat, lon]).addTo(map);
-};
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; OpenStreetMap contributors'
+        }).addTo(map);
+    }
+    updateMarkers(initialMarkers);
+}
+
+function updateMarkers(markers) {
+    if (!map) return;
+
+    // Remove existing markers
+    map.eachLayer((layer) => {
+        if (layer instanceof L.Marker) map.removeLayer(layer);
+    });
+
+    // Add new markers
+    markers.forEach(m => {
+        L.marker([m.latitude, m.longitude])
+            .addTo(map)
+            .bindPopup(m.info);
+    });
+
+    // Optional: Adjust map view to fit markers
+    if (markers.length > 0) {
+        const bounds = L.latLngBounds(markers.map(m => [m.latitude, m.longitude]));
+        map.fitBounds(bounds, { padding: [50, 50] });
+    }
+}
