400cd0d0389731889e5d5538b74a623273ab7875
Sandro Räss <s.raess@me.com>
2026-01-02 14:02:59 +0100

    Add RoutePlanner.

---
+++ MauiBlazorWeb/MauiBlazorWeb.Shared/Classes/DriverRoutePlan.cs
@@ -0,0 +1,25 @@
+﻿using Shared.Dtos.DtosImpl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBlazorWeb.Shared.Classes
+{
+    public class DriverRoutePlan
+    {
+        public DriverDto Driver { get; set; }
+        public List<SaveChickenRequestDto> SaveChickenRequests { get; set; }
+
+        public List<MapMarker> RouteMarkers { get; set; } = new List<MapMarker>();
+
+        public List<MapArrow> RouteArrows { get; set; } = new List<MapArrow>();
+
+        public DriverRoutePlan() { 
+            Driver = new DriverDto();
+            SaveChickenRequests = new List<SaveChickenRequestDto>();
+        }
+
+    }
+}
